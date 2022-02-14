4e2e22be6432904c995137a7179684252a75d9eb
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-14 11:21:54 +0800

    时间与排序

--- main.py
+++ main.py
@@ -2,7 +2,25 @@
 
 import numpy
 import talib
+import json
+import datetime
 
-close = numpy.random.random(100)
-output = talib.MA(close, 44)
-print(output)
+data = json.load(open('btcusd.json', 'r'))
+
+# a = dict.items(data.__dict__)
+
+list = []
+for key, value in data['data']['points'].items():
+  list.append({
+    'time': int(key),
+    'value': value,
+  })
+list.sort(key = lambda x: x['time'], reverse = True)
+for item in list:
+  print(datetime.datetime.fromtimestamp(item['time']))
+
+# print(len(data['data']['points']))
+
+# close = numpy.random.random(100)
+# output = talib.MA(close, 44)
+# print(output)
